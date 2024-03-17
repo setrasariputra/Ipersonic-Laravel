70002e1eb5c2bff879247da82b92bea7a173fb24
Wisnu K. Ardianto <setrasariputra@gmail.com>
2024-03-17 20:48:58 +0700

    IPL-5: Membuat Link Dropdown Result

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,6 +1,6 @@
 @include('partials.head')
 <body>
-    @include('partials.navbar')
+    @include('partials.navbar', ['defineResult' => $defineResult])
     <main class="pt-4 pb-4">
         <div class="container">
             <h1 class="display-6"><strong>{{ $result['label'] }}</strong></h1>
