1956c8694f8a182ac38fe1364ac17e295484078a
Wisnu K. Ardianto <setrasariputra@gmail.com>
2024-03-17 10:56:38 +0700

    IPL-1: Membuat Halaman Welcome Page

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+    <div class="container">
+        <div>
+            <a class="navbar-brand" href="#">iPersonic</a>
+            <a href="#" class="btn btn-sm btn-primary">Mulai Test <i class="bi bi-caret-right-fill"></i></a>
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Tipe Kepribadian
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Idealis Penyelaras</a></li>
+                        <li><a class="dropdown-item" href="#">Idealis Terlibat</a></li>
+                        <li><a class="dropdown-item" href="#">Pemikir Dinamis</a></li>
+                        <li><a class="dropdown-item" href="#">Pemikir Pendobrak</a></li>
+                        <li><a class="dropdown-item" href="#">Idealis Spontan</a></li>
+                        <li><a class="dropdown-item" href="#">Pelaku Santai</a></li>
+                        <li><a class="dropdown-item" href="#">Realis Sosialis</a></li>
+                        <li><a class="dropdown-item" href="#">Realis Bertekad</a></li>
+                        <li><a class="dropdown-item" href="#">Pelaku Bersemangat</a></li>
+                        <li><a class="dropdown-item" href="#">Pelaku Individualis</a></li>
+                        <li><a class="dropdown-item" href="#">Pelaku Peka</a></li>
+                        <li><a class="dropdown-item" href="#">Realis Baik Hati</a></li>
+                        <li><a class="dropdown-item" href="#">Realis Terpercaya</a></li>
+                        <li><a class="dropdown-item" href="#">Pemikir Mandiri</a></li>
+                        <li><a class="dropdown-item" href="#">Pemikir Analitis</a></li>
+                        <li><a class="dropdown-item" href="#">Idealis Pemimpi</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
